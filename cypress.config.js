40d06d449c3cd17d10fc139edbba55230e7362bb
wajih2411 <wajihullahm@gmail.com>
2024-01-05 12:27:57 -0600

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,12 +5,4 @@ module.exports = {
     },
   },
   defaultCommandTimeout: 20000,
-  const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  e2e: {
-    supportFile: false,
-    // ... other configurations
-  },
-});
 };
