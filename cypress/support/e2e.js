38cf96a66978d961369f54856d3bf690c28c21d1
wajih2411 <wajihullahm@gmail.com>
2024-01-12 16:51:02 -0600

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,7 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './support/commands'
+import './support/commands.js'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
